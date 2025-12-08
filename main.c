32a109a6fd15ddbed376ee07943e589e63fea97d
ferasahmed285 <23p0304@eng.asu.edu.eg>
2025-12-08 14:12:05 +0200

    start

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/systick.h"
+#include "driverlib/interrupt.h"
+int main()
+{
+  return 0;
+}
