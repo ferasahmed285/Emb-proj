cc9d594ced81c310534f5ee4e5452980ba53c5ba
ferasahmed285 <23p0304@eng.asu.edu.eg>
2025-12-15 14:43:40 +0200

    motor and buzzer test

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 
+// Function prototypes
+void enable_buzzer(void);
+void enable_motor(void);
+void buzzer_test(void);
+void motor_test(void);
+
 int main(void)
 {
   //
@@ -12,10 +18,48 @@ int main(void)
   //
   SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
                  SYSCTL_OSC_MAIN);
+  enable_buzzer();
+  enable_motor();
+  while (1)
+  {
+    buzzer_test();
+    SysCtlDelay(16666667);
+    motor_test();
+  }
+}
+
+//
+// Function to enable and configure the buzzer
+//
+void enable_buzzer(void)
+{
+  //
+  // Enable GPIO Port B (Buzzer).
+  //
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
   //
-  // Enable the GPIO Port D.
-  // IN1 is connected to PD0, IN2 is connected to PD1.
+  // Wait for the GPIO module to be ready.
+  //
+  while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB))
+  {
+  }
+
+  //
+  // Configure PB7 as an output for the buzzer.
+  //
+  GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_7);
+  // High drive strength for buzzer
+  GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_7, GPIO_STRENGTH_8MA_SC, GPIO_PIN_TYPE_STD);
+}
+
+//
+// Function to enable and configure the motor
+//
+void enable_motor(void)
+{
+  //
+  // Enable GPIO Port D (Motor).
   //
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
@@ -27,41 +71,49 @@ int main(void)
   }
 
   //
-  // Configure PD0 and PD1 as outputs.
+  // Configure PD0 (IN1) and PD1 (IN2) as outputs for the motor.
   //
   GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+}
 
-  //
-  // Loop forever.
-  //
-  while (1)
-  {
-    //
-    // 1. Turn Right (1 second)
-    // IN1 (PD0) = High, IN2 (PD1) = Low
-    //
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_PIN_0);
+//
+// Function to test the buzzer
+// Turns on buzzer for 1 second
+//
+void buzzer_test(void)
+{
+  // Turn on buzzer (PB7 High)
+  GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_7, GPIO_PIN_7);
 
-    // Delay 1 second
-    // (1 * 50,000,000) / 3 = 16,666,667
-    SysCtlDelay(16666667);
+  // Delay 1 second
+  SysCtlDelay(16666667);
 
-    //
-    // 2. Stop (1 second)
-    // IN1 (PD0) = Low, IN2 (PD1) = Low
-    //
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0);
+  // Turn off buzzer
+  GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_7, 0);
+}
 
-    // Delay 1 second
-    SysCtlDelay(16666667);
+//
+// Function to test the motor
+// Turn right 1s -> Wait 5s -> Turn left 1s
+//
+void motor_test(void)
+{
+  // 1. Turn Right (IN1 High, IN2 Low)
+  GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_PIN_0);
 
-    //
-    // 3. Turn Left (1 second)
-    // IN1 (PD0) = Low, IN2 (PD1) = High
-    //
-    GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_PIN_1);
+  // Run for 1 second
+  SysCtlDelay(16666667);
 
-    // Delay 1 second
-    SysCtlDelay(16666667);
-  }
+  // 2. Wait 5 seconds (Stop motor)
+  GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0);
+  SysCtlDelay(16666667 * 5);
+
+  // 3. Turn Left (IN1 Low, IN2 High)
+  GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_PIN_1);
+
+  // Run for 1 second
+  SysCtlDelay(16666667);
+
+  // Stop motor after sequence
+  GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0);
 }
